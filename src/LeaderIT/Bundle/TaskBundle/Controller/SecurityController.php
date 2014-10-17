e49197a7f2e2d44e06b030d18e751fa29cba78c7
mazhuravlev_____________ <m.zhuravlev61@gmail.com>
2014-10-17 15:45:10 +0700

    Добавлено security

---
+++ src/LeaderIT/Bundle/TaskBundle/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LeaderIT\Bundle\TaskBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\Time;
+
+
+class SecurityController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        $result =  array(
+            'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+
+        return $this->render('LeaderITTaskBundle:Security:login.html.twig', $result);
+    }
+
+    public function checkAction() {
+        // The security layer will intercept this request
+    }
+
+    public  function logoutAction() {
+        // The security layer will intercept this request
+    }
+}
